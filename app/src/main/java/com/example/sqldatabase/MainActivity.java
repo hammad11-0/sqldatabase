148fc5d72da28271c967cee5b95829ec69bd5d1b
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-12-21 21:26:14 +0500

    main

--- app/src/main/java/com/example/sqldatabase/MainActivity.java
+++ app/src/main/java/com/example/sqldatabase/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.sqldatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView t;
+    EditText p1;
+    Button b1;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t=findViewById(R.id.textView);
+        p1=findViewById(R.id.editTextPhone);
+        b1 = findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
     }
 }
