d6db4799a8305fb9a56f87720f44db67b01c3b22
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-12-21 20:58:46 +0500

    student model

---
+++ app/src/androidTest/java/student.java
@@ -0,0 +1,32 @@
+public class student {
+
+    private String name;
+    private int phno;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhno() {
+        return phno;
+    }
+
+    public void setPhno(int phno) {
+        this.phno = phno;
+    }
+
+    public student(String s,int a)
+    {
+        this.name=s;
+        this.phno=a;
+
+    }
+
+
+
+}
